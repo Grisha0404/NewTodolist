879cfaea9827dfcef6e6ca70c597f71328cd08d8
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-05 16:41:51 +0300

    Update TodoListHeader.tsx

--- src/TodoListHeader.tsx
+++ src/TodoListHeader.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import AddTaskFrom from "./AddTaskFrom";
 
-type TodoListHeaderType ={
+type TodoListHeaderType = {
     title: string
+
 }
 
-const TodoListHeader = (props:TodoListHeaderType) => {
+const TodoListHeader = (props: TodoListHeaderType) => {
     return (
         <div>
-                <h3>{props.title}</h3>
-                <AddTaskFrom/>
+            <h3>{props.title}</h3>
+            <AddTaskFrom/>
         </div>
     );
 };
