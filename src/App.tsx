53576d02adbdfefd7bb9371ea2235f28f2a403e5
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-08 17:54:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,40 @@ import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
 
-export type FilterValuesType =
-    'all' | 'active' | 'completed';
+export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
-    let [task_1, setTask1] = useState<Array<TaskType>>([
+    let [task, setTask] = useState<Array<TaskType>>([
         {id: 1, title: 'HTML', isDone: true},
         {id: 2, title: 'CSS', isDone: false},
-        {id: 3, title: 'JS/TS', isDone: true}
+        {id: 3, title: 'JS/TS', isDone: true},
+        {id: 4, title: 'REACT', isDone: false}
     ])
+    let [filter, setFilter] = useState<FilterType>('all');
 
-    let [filter1, setFilter1] = useState<FilterValuesType>('all')
-
-    let removeTask1 = (id: number) => {
-        let filterTask = task_1.filter(task => task.id !== id);
-        setTask1(filterTask);
-    }
-
-    const taskForTodoList1 = () => {
-        switch (filter1) {
-            case 'completed':
-                return task_1.filter(t => t.isDone);
-            case 'active':
-                return task_1.filter(t => !t.isDone);
+    const taskForTodoList = () => {
+        switch (filter) {
+            case "active":
+                return task.filter(f => !f.isDone)
+            case "completed":
+                return task.filter(f => f.isDone)
             default:
-                return task_1;
+                return task
         }
     }
-    let changeFilter1 = (value: FilterValuesType) => {
-        setFilter1(value);
+    let changeFilter = (filter: FilterType) => {
+        setFilter(filter)
+    }
+
+    let removeTask = (id: number) => {
+        let filterTask = task.filter(t => t.id !== id);
+        setTask(filterTask)
     }
 
     return (
         <div className="App">
-            <TodoList title={'What to learn'} task={taskForTodoList1()} removeTask={removeTask1}
-                      changeFilter={changeFilter1}/>
+            <TodoList title={'What to learn'} task={taskForTodoList()} removeTask={removeTask}
+                      changeFilter={changeFilter}/>
             {/*<TodoList title={'What to read'} task={taskForTodoList2} removeTask={removeTask2}
                       changeFilter={changeFilter2}/>
             <TodoList title={'What to write'} task={taskForTodoList3} removeTask={removeTask3}
