53e8749bf8b7b94fe403ce3cd103f992e7f9fc5f
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-08 17:54:46 +0300

    Update TodoList.tsx

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import TodoListHeader from "./TodoListHeader";
 import TaskList from "./TaskList";
-import {FilterValuesType} from "./App";
+import {FilterType} from "./App";
 
 
 export type TodoListPopsType = {
     title: string,
     task: Array<TaskType>
     removeTask: (id: number) => void
-    changeFilter: (value: FilterValuesType) => void
+    changeFilter: (filter: FilterType) => void
 }
 
 export type TaskType = {
