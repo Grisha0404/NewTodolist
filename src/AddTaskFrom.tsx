83387fe12b1dff656452910c98591755f8e65e34
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-14 17:41:09 +0300

    Update AddTaskFrom.tsx

--- src/AddTaskFrom.tsx
+++ src/AddTaskFrom.tsx
@@ -1,27 +1,15 @@
-import React, {ChangeEvent, useState} from 'react';
-import style from "./todoList.module.css";
-
+import React from 'react';
 
 type AddTaskType = {
-    addNewTask: (title: string) => void
+
 }
 
 const AddTaskFrom = (props: AddTaskType) => {
 
-    let [title, setTitle] = useState('')
-
-    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value)
-    }
-    const onClickButtonHandler = () => {
-        props.addNewTask(title)
-        setTitle('')
-    }
-
     return (
         <div>
-            <input value={title} onChange={onChangeInputHandler}/>
-            <button className={style.buttonAdd} onClick={onClickButtonHandler}>Add</button>
+            <input  />
+            <button>Add</button>
         </div>
     );
 };
