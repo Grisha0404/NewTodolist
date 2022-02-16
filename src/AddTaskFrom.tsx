d5ae77f0d4e9be9d32bde987dd000a9bf2f90c37
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-16 15:12:47 +0300

    Update AddTaskFrom.tsx

--- src/AddTaskFrom.tsx
+++ src/AddTaskFrom.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 
 type AddTaskFromType = {
     addTask: (title: string) => void
 }
 
 const AddTaskFrom = (props: AddTaskFromType) => {
-
+    const [title, setTitle] = useState('')
+    const onClickInputHandler =(event: ChangeEvent<HTMLInputElement>)=> {
+        setTitle(event.currentTarget.value)
+    }
     const onClickButtonHandler = () => {
-        props.addTask("title")
+        props.addTask(title)
+        setTitle('')
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        e.key === 'Enter' && onClickButtonHandler()
     }
     return (
         <div>
-            <input/>
+            <input value={title} onChange={onClickInputHandler} onKeyPress={onKeyPressHandler}/>
             <button onClick={onClickButtonHandler}>Add</button>
         </div>
     );
