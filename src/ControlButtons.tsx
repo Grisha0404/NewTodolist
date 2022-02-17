c8327fc50d8f74c2ea5e9622767b337e789f1ebe
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-17 22:33:07 +0300

    Update ControlButtons.tsx

--- src/ControlButtons.tsx
+++ src/ControlButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FilterType} from "./App";
-import style from "./todoList.module.css"
+import Button from "./Button";
 
 type ButtonsType = {
     changeFilter: (filter: FilterType) => void
@@ -10,18 +10,21 @@ const ControlButtons = (props: ButtonsType) => {
     /*const allHandler = () => props.changeFilter('all');
     const activeHandler = () => props.changeFilter('active');
     const completedHandler = () => props.changeFilter('completed');*/
-    const onClickHandler = (filter: FilterType) => () =>
-        props.changeFilter(filter)
+    /* const onClickHandler = (filter: FilterType) => () =>
+         props.changeFilter(filter)*/
 
 
     return (
         <div>
-            <button className={style.button} onClick={onClickHandler('all')}>All
+            {/*<button className={style.button} onClick={onClickHandler('all')}>All
             </button>
             <button className={style.button} onClick={onClickHandler('active')}>Active
             </button>
             <button className={style.button} onClick={onClickHandler('completed')}>Completed
-            </button>
+            </button>*/}
+            <Button name={'All'} callback={() => props.changeFilter('all')}/>
+            <Button name={'Active'} callback={() => props.changeFilter('active')}/>
+            <Button name={'Completed'} callback={() => props.changeFilter('completed')}/>
         </div>
     );
 };
