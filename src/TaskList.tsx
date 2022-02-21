5bb5f5a9d73f029d1f96ee03435f2c68d5f9bc82
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-21 20:21:27 +0300

    Update TaskList.tsx

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -9,18 +9,22 @@ type TaskListType = {
     tasks: Array<TaskType>
     removeTask: (id: string) => void
     changeFilter: (filter: FilterType) => void
+    filter: FilterType
 }
 
 
 const TaskList = (props: TaskListType) => {
     const tasksHandler = props.tasks.map(t => <Tasks key={t.id} {...t} removeTask={props.removeTask}/>)
+    const taskList = tasksHandler.length
+        ? <ul>
+            {tasksHandler}
+        </ul>
+        : <span style={{fontSize: '10px', color: 'red'}}> Enter your task please </span>
     return (
-        <div>
-            <ul>
-                {tasksHandler}
-            </ul>
-            <ControlButtons changeFilter={props.changeFilter}/>
-        </div>
+        <>
+            {taskList}
+            <ControlButtons changeFilter={props.changeFilter} filter={props.filter}/>
+        </>
     );
 };
 
