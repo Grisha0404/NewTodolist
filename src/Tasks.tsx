3394b42398a77c091f2e3f43a79019f53d075293
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-02-22 19:59:57 +0300

    Update Tasks.tsx

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {TaskType} from "./TodoList";
 import Button from "./Button";
 
 type TaskPropsType = TaskType & {
     removeTask: (id: string) => void
+    changeTaskStatus: (id: string, isDune: boolean) => void
 }
 
 const Tasks = (props: TaskPropsType) => {
 
     const taskClass = props.isDone ? 'completed-task' : '';
     const buttonHandler = () => props.removeTask(props.id);
+    const changeTskStatus = (e: ChangeEvent<HTMLInputElement>)=>
+    {props.changeTaskStatus(props.id,e.currentTarget.checked)}
 
     return (
         <div>
             {
                 <li>
-                    <input type={"checkbox"} checked={props.isDone}/>
+                    <input type={"checkbox"}
+                           onChange={changeTskStatus}
+                               checked={props.isDone}/>
                     <span className={taskClass}>{props.title}</span>
                     <Button classNameBut={''} name={'x'} callback={buttonHandler}/>
                 </li>
