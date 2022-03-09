591d3a72a5caabc41c6c8534225f3c57ebd8b5b3
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-09 22:04:21 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
+import {ModelWindow} from "./components/ModelWindow";
 import {AddItemForm} from "./components/AddItemForm";
 
 export type FilterValuesType = "all" | "active" | "completed";
@@ -35,6 +36,8 @@ function App() {
             {id: v1(), title: "React Book", isDone: true}
         ]
     });
+
+
     function removeTask(id: string, todolistId: string) {
         //достанем нужный массив по todolistId:
         let todolistTasks = tasks[todolistId];
@@ -43,6 +46,7 @@ function App() {
         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
         setTasks({...tasks});
     }
+
     function addTask(title: string, todolistId: string) {
         let task = {id: v1(), title: title, isDone: false};
         //достанем нужный массив по todolistId:
@@ -52,6 +56,7 @@ function App() {
         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
         setTasks({...tasks});
     }
+
     function changeStatus(id: string, isDone: boolean, todolistId: string) {
         //достанем нужный массив по todolistId:
         let todolistTasks = tasks[todolistId];
@@ -64,6 +69,7 @@ function App() {
             setTasks({...tasks});
         }
     }
+
     function changeFilter(value: FilterValuesType, todolistId: string) {
         let todolist = todolists.find(tl => tl.id === todolistId);
         if (todolist) {
@@ -71,6 +77,7 @@ function App() {
             setTodolists([...todolists])
         }
     }
+
     function removeTodolist(id: string) {
         // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
         setTodolists(todolists.filter(tl => tl.id != id));
@@ -79,32 +86,27 @@ function App() {
         // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
         setTasks({...tasks});
     }
-    const upDateTask = (todolistId: string,taskId: string,title:string)=>{
-        setTasks({...tasks, [todolistId]:[...tasks[todolistId].map(el=> el.id===taskId ? {...el, title: title}: el)]})
-    }
-    const addTodolist = (newTitle:string) =>{
-        const newID = v1();
-        let newTodolist: TodolistType = {id: newID, title: newTitle, filter: "all"}
+
+    const addTodolistHandler = (title: string) => {
+        let newID = v1()
+        let newTodolist:TodolistType = {id:newID, title: title, filter: "all"}
         setTodolists([newTodolist,...todolists])
-        setTasks({...tasks, [newID]: []})
-    }
-    const updateTitle = (todolistId: string,title: string)=>{
-        setTodolists(todolists.map(el=> el.id===todolistId ?{...el, title}:el))
+        setTasks({[newID]: [],...tasks})
     }
 
     return (
         <div className="App">
-            <AddItemForm callBack={addTodolist}/>
+            <AddItemForm callBack={addTodolistHandler}/>
             {
                 todolists.map(tl => {
                     let allTodolistTasks = tasks[tl.id];
                     let tasksForTodolist = allTodolistTasks;
 
                     if (tl.filter === "active") {
-                        tasksForTodolist = allTodolistTasks.filter(t => !t.isDone);
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
                     }
                     if (tl.filter === "completed") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone);
+                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
                     }
 
                     return <Todolist
@@ -117,9 +119,7 @@ function App() {
                         addTask={addTask}
                         changeTaskStatus={changeStatus}
                         filter={tl.filter}
-                        upDateTask={upDateTask}
                         removeTodolist={removeTodolist}
-                        updateTitle={updateTitle}
                     />
                 })
             }
