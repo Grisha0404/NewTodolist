2e8d1f33dddb1758a8af4003ae449abe21396e9c
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-03-09 22:04:37 +0300

    Update EditableSpan.tsx

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -1,32 +1,9 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React from 'react';
 
-type EditableSpanType={
-    oldTitle:string
-    callBack:(title:string)=>void
-}
-
-export const EditableSpan = (props:EditableSpanType) => {
-    let [edit, setEdit] = useState(false)
-    let [newTitle, setNewTitle] = useState(props.oldTitle)
-
-    const doubleClickHandler = () => {
-      setEdit(true)
-    }
-    const blurHandler = () => {
-        props.callBack(newTitle)
-      setEdit(false)
-    }
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewTitle(e.currentTarget.value)
-    }
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            blurHandler();
-        }
-    }
-    
+export const EditableSpan = () => {
     return (
-        edit ? <input value={newTitle} onChange={onChangeHandler} autoFocus onBlur={blurHandler} onKeyPress={onKeyPressHandler}/> :
-        <span onDoubleClick={doubleClickHandler}>{props.oldTitle}</span>
+        <div>
+
+        </div>
     );
 };
