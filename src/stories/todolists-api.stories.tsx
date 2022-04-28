df755cbc5e929a621a8e344c096dde2cc7e9c96f
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-28 22:24:06 +0300

    les 13

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react'
+import {todoListApi} from "../api/todolistAPI";
+
+export default {
+    title: 'API'
+}
+
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todoListApi.getTodos()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todoListApi.createTodos()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todoListApi.deleteTodos()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todoListApi.updateTodos()
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
