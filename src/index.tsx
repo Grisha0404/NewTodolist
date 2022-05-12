75286aa9466f9c5c453f7a9621c1cfd960fa32da
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-12 20:24:42 +0300

    les 15

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './app/App';
 import { Provider } from 'react-redux';
-import { store } from './state/store';
+import { store } from './app/store';
 
 ReactDOM.render(
-
+    <React.StrictMode>
         <Provider store={store}>
             <App/>
-        </Provider>,
+        </Provider>
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
