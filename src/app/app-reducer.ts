260b47c7e70039f02d8666a37f6fe8a0c958f00d
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-19 23:11:36 +0300

    les 16

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,26 +1,40 @@
-export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
-
-const initialState = {
-    status: 'loading' as RequestStatusType,
-    error: 'error' as string | null
+const initialState: InitialStateType = {
+    status: 'idle',
+    error: null,
+    isInitialized: false
 }
-
-type InitialStateType = typeof initialState
 
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
-        case "APP/SET-ERROR":
+        case 'APP/SET-ERROR':
             return {...state, error: action.error}
+        case 'APP/SET-INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
         default:
-            return state
+            return {...state}
     }
 }
 
-export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const);
-export const setErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const);
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    // происходит ли сейчас взаимодействие с сервером
+    status: RequestStatusType
+    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+    error: string | null,
+    isInitialized: boolean
+}
 
-export type ActionsType = SetAppStatusACType | SetErrorACType
-export type SetAppStatusACType = ReturnType<typeof setAppStatusAC>
-export type SetErrorACType = ReturnType<typeof setErrorAC>
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppIsInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-INITIALIZED', isInitialized} as const)
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppIsInitializedACType = ReturnType<typeof setAppIsInitializedAC>
+
+type ActionsType =
+    | SetAppErrorActionType
+    | SetAppStatusActionType
+    | SetAppIsInitializedACType
